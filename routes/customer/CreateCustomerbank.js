74a637baae971f75e5d76d877c6c04f98d725f9b
cbkapil <103408292+cbkapil@users.noreply.github.com>
2023-01-09 15:14:06 +0530

    to deploy on Server

---
+++ routes/customer/CreateCustomerbank.js
@@ -0,0 +1,46 @@
+
+import Party from '../../models/PartyModel.js'
+import express from "express";
+import auth from "../Auth.js";
+const router = express.Router();
+
+router.post("/createpartybank", auth, async (req, res) => {
+  console.log("xxxxxxxxxxxxxx", req.userId);
+  const {
+   formValues
+  } = req.body;
+  console.log("formvaluessssssssssssssssss",formValues);
+  try {
+  formValues.map(user => {
+      console.log("useridddddd",user._id);
+      return new Promise((resolve, reject) => {
+        var banklink = Party.findByIdAndUpdate(
+            user.customerid,
+            { $push: { customerbank: [{
+              partybankname : user.customerbank,
+              partybranch : user.customerbranch
+            }]} },
+            function (err, result) {
+            if (err)
+             {
+              reject(  res.json({ message: "Failed to Add Bank", status: false }))
+            }
+            else{
+            console.log("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+              resolve()
+              
+             
+            }}
+          );
+        
+       
+      })
+    });
+
+   
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+export default router;
